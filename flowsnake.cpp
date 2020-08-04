9b2cc346a98cca4ec016ca6a1bf3cc0479f8ae0a
Pierre Abbat <phma@bezitopo.org>
2020-08-04 05:48:38 +0000

    Add flowsnake.h and flowsnake.cpp

---
+++ flowsnake.cpp
@@ -0,0 +1,13 @@
+/******************************************************/
+/*                                                    */
+/* flowsnake.cpp - flowsnake                          */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020 Pierre Abbat
+ * Licensed under the Apache License, Version 2.0.
+ */
+#include "flowsnake.h"
+using namespace std;
+
+const Eisenstein flowBase(2,-1);
+const Eisenstein flowBaseConj(3,1);
---
+++ flowsnake.h
@@ -0,0 +1,9 @@
+/******************************************************/
+/*                                                    */
+/* flowsnake.h - flowsnake                            */
+/*                                                    */
+/******************************************************/
+/* Copyright 2020 Pierre Abbat
+ * Licensed under the Apache License, Version 2.0.
+ */
+#include "eisenstein.h"
