1fb6b96482b94c3c31c4d54c9dcd102770c81bd8
Pierre Abbat <phma@bezitopo.org>
2021-02-11 07:41:32 +0000

    Try to express i as a flowsnake number

--- flowbound.cpp
+++ flowbound.cpp
@@ -13,15 +13,16 @@ int main(int argc, char *argv[])
   int i,j;
   FlowNumber a("261"),b("26.1"),c("2.61"),d(".261"),e("0.261"),f("0000000000002.61000000000000");
   FlowNumber p("14"),rp(".1111111111111111111111");
+  FlowNumber I("2.3432");
   init();
   fillTables();
   //testTables();
-  for (i=0;i<42;i++)
+  /*for (i=0;i<42;i++)
   {
     refine();
     prune();
     cout<<i<<' '<<boundary.size()<<endl;
-  }
+  }*/
   for (i=0;i<boundary.size();i++)
   {
     cout<<toBase7(boundary[i].a.cartx())<<','<<toBase7(boundary[i].a.carty())<<endl;
@@ -38,5 +39,7 @@ int main(int argc, char *argv[])
   cout<<(a-e).toString()<<' '<<(complex<double>)(a-e)<<endl;
   cout<<(a*e).toString()<<' '<<(complex<double>)(a*e)<<endl;
   cout<<(p*rp).toString()<<' '<<(complex<double>)(p*rp)<<endl;
+  cout<<(I).toString()<<' '<<(complex<double>)(I)<<endl;
+  cout<<(I*I).toString()<<' '<<(complex<double>)(I*I)<<endl;
   return 0;
 }
